fa91c48a59c6e70f2b3fb695f591821b8d8011c1
PetirrzZzC <ananda_prasetyo_34pg@student.smktelkom-mlg.sch.id>
2026-01-14 09:55:51 +0700

    Latihan Method2
    non void menggunakan variabel decimal

---
+++ LatihanMethod2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+using System.Text.Json.Serialization;
+
+int jumlahItem;
+
+decimal hargaItem, diskon, totalBayar;
+
+static decimal hitungDiskon(int jml_item)
+{
+    if (jml_item == 5)
+        return 50000;
+    else if (jml_item == 10)
+        return 75000;
+    else
+        return 0;
+}
+
+static decimal hitungTotal(int jml_item, decimal hrg_item, decimal diskon_item)
+{
+    return jml_item * hrg_item - diskon_item;
+}
+
+Console.Write("Masukkan Jumlah item : ");
+jumlahItem = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write("Masukkan harga per item : ");
+hargaItem = decimal.Parse(Console.ReadLine());
+
+diskon = hitungDiskon(jumlahItem);
+
+totalBayar = hitungTotal(jumlahItem, hargaItem, diskon);
+Console.WriteLine($"Total yang harus dibayar adalah : {totalBayar}");
